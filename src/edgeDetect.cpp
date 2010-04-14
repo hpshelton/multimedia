503a522711e61a70ce201150d68ef81b93fdc2cf
Parker Shelton <hpshelton@Parkers-Computer.local>
2010-04-14 00:35:48 -0400

    Implemented CPU edge-detection

--- src/edgeDetect.cpp
+++ src/edgeDetect.cpp
@@ -5,6 +5,8 @@
 QImage* MainWindow::edge_detect()
 {
 	QImage* img = this->file[0];
+	int width = img->width();
+	int height = img->height();
 
 	if(CUDA_CAPABLE && CUDA_ENABLED)
 	{
@@ -16,7 +18,7 @@ QImage* MainWindow::edge_detect()
 		cutilSafeCall(cudaMalloc((void**)&CUoutput, memSize));
 
 		cutilSafeCall(cudaMemcpy(CUinput, img->bits(), memSize, cudaMemcpyHostToDevice));
-		edgeDetectGPU_rgba(CUoutput, CUinput, img->height(), img->width());
+		edgeDetectGPU_rgba(CUoutput, CUinput, height, width);
 		cutilSafeCall(cudaMemcpy(img->bits(), CUoutput, memSize, cudaMemcpyDeviceToHost));
 
 		cutilSafeCall(cudaFree(CUinput));
@@ -26,39 +28,156 @@ QImage* MainWindow::edge_detect()
 	}
 	else
 	{
+		QImage* newImg = new QImage(*img);
 
-	}
+		// Weight masks
+		float mask[3][3] = { {-1.0, -1.0, -1.0}, {-1.0, 8.0, -1.0}, {-1.0, -1.0, -1.0} };
+		float maskEdge[3][3]= { {-1.0, -1.0, -1.0}, {-1.0, 5.0, -1.0}, {-1.0, -1.0, -1.0} };
+		float maskCorner[3][3] = { {-1.0, -1.0, -1.0}, {-1.0, 3.0, -1.0}, {-1.0, -1.0, -1.0} };
 
-/*	float coeff[3][3] = {{-1, -1, -1},
-							 {-1,  8, -1},
-							 {-1, -1, -1}};
-
-	int i, j;
-	float convSumR, convSumG, convSumB;
-
-	QImage* img = this->file[0];
-
-	for(int y = 0; y < img->height(); y++)
-	{
-		for(int x = 0; x < img->width(); x++)
+		for(int y = 0; y < height; y++)
 		{
-			convSumR=0;
-			convSumG=0;
-			convSumB=0;
-			for(i=-1; i < 2; i++){
-				for(j=-1; j < 2; j++){
-					if( 0<=(x+i) && (x+i) < img->width() && 0 <= (y+j) && (y+j) < img->height() ){
-						convSumR += (coeff[j+1][i+1] * qRed  (img->pixel(x+i,y+j)));
-						convSumG += (coeff[j+1][i+1] * qGreen(img->pixel(x+i,y+j)));
-						convSumB += (coeff[j+1][i+1] * qBlue (img->pixel(x+i,y+j)));
+			for(int x = 0; x < width; x++)
+			{
+				int poutr = 0, poutg = 0, poutb = 0;
+
+				if(x == 0)
+				{
+					if(y == 0) // top left corner
+					{
+						for(int i = 1; i <= 2; i++)
+						{
+							for(int j = 1; j <= 2; j++)
+							{
+								QRgb pixel = img->pixel(x+(j-1), y+(i-1));
+								poutr += maskCorner[i][j]*qRed(pixel);
+								poutg += maskCorner[i][j]*qGreen(pixel);
+								poutb += maskCorner[i][j]*qBlue(pixel);
+							}
+						}
+					}
+					else if(y == height - 1) // bottom left corner
+					{
+						for(int i = 0; i <= 1; i++)
+						{
+							for(int j = 1; j <= 2; j++)
+							{
+								QRgb pixel = img->pixel(x+(j-1), y+(i-1));
+								poutr += maskCorner[i][j]*qRed(pixel);
+								poutg += maskCorner[i][j]*qGreen(pixel);
+								poutb += maskCorner[i][j]*qBlue(pixel);
+							}
+						}
+					}
+					else // left edge
+					{
+						for(int i = 0; i <= 2; i++)
+						{
+							for(int j = 1; j <= 2; j++)
+							{
+								QRgb pixel = img->pixel(x+(j-1), y+(i-1));
+								poutr += maskEdge[i][j]*qRed(pixel);
+								poutg += maskEdge[i][j]*qGreen(pixel);
+								poutb += maskEdge[i][j]*qBlue(pixel);
+							}
+						}
 					}
 				}
+				else if(x == width - 1)
+				{
+					if(y == 0) // top right corner
+					{
+						for(int i = 1; i <= 2; i++)
+						{
+							for(int j = 0; j <= 1; j++)
+							{
+								QRgb pixel = img->pixel(x+(j-1), y+(i-1));
+								poutr += maskCorner[i][j]*qRed(pixel);
+								poutg += maskCorner[i][j]*qGreen(pixel);
+								poutb += maskCorner[i][j]*qBlue(pixel);
+							}
+						}
+					}
+					else if(y == height - 1) // bottom right corner
+					{
+						for(int i = 0; i <= 1; i++)
+						{
+							for(int j = 0; j <= 1; j++)
+							{
+								QRgb pixel = img->pixel(x+(j-1), y+(i-1));
+								poutr += maskCorner[i][j]*qRed(pixel);
+								poutg += maskCorner[i][j]*qGreen(pixel);
+								poutb += maskCorner[i][j]*qBlue(pixel);
+							}
+						}
+					}
+					else // right edge
+					{
+						for(int i = 0; i <= 2; i++)
+						{
+							for(int j = 0; j <= 1; j++)
+							{
+								QRgb pixel = img->pixel(x+(j-1), y+(i-1));
+								poutr += maskEdge[i][j]*qRed(pixel);
+								poutg += maskEdge[i][j]*qGreen(pixel);
+								poutb += maskEdge[i][j]*qBlue(pixel);
+							}
+						}
+					}
+				}
+				else // 0 < x < width
+				{
+					if(y == 0) // top edge
+					{
+						for(int i = 1; i <= 2; i++)
+						{
+							for(int j = 0; j <= 2; j++)
+							{
+								QRgb pixel = img->pixel(x+(j-1), y+(i-1));
+								poutr += maskEdge[i][j]*qRed(pixel);
+								poutg += maskEdge[i][j]*qGreen(pixel);
+								poutb += maskEdge[i][j]*qBlue(pixel);
+							}
+						}
+					}
+					else if(y == height - 1) // bottom edge
+					{
+						for(int i = 0; i <= 1; i++)
+						{
+							for(int j = 0; j <= 2; j++)
+							{
+								QRgb pixel = img->pixel(x+(j-1), y+(i-1));
+								poutr += maskEdge[i][j]*qRed(pixel);
+								poutg += maskEdge[i][j]*qGreen(pixel);
+								poutb += maskEdge[i][j]*qBlue(pixel);
+							}
+						}
+					}
+					else // middle
+					{
+						for(int i = 0; i <= 2; i++)
+						{
+							for(int j = 0; j <= 2; j++)
+							{
+								QRgb pixel = img->pixel(x+(j-1), y+(i-1));
+								poutr += mask[i][j]*qRed(pixel);
+								poutg += mask[i][j]*qGreen(pixel);
+								poutb += mask[i][j]*qBlue(pixel);
+							}
+						}
+					}
+				}
+
+				poutr = CLAMP(poutr);
+				poutg = CLAMP(poutg);
+				poutb = CLAMP(poutb);
+
+				newImg->setPixel(x, y, qRgb(poutr, poutg, poutb));
 			}
-			img->setPixel(x,y,qRgb(CLAMP(convSumR),CLAMP(convSumG),CLAMP(convSumB)));
 		}
+		return newImg;
 	}
-	return img;
-*/}
+}
 
 QImage* MainWindow::edge_detect_video()
 {
