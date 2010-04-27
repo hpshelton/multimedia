f21631166e74fd81a9b7f85ce84e7d4e5882309c
hpshelton <parker.shelton@jhu.edu>
2010-04-27 15:44:45 -0400

    Renamed DocumentDisplay to ImageDisplay. Created VideoDisplay class.

---
+++ src/videodisplay.cpp
@@ -0,0 +1,5 @@
+#include "videodisplay.h"
+
+VideoDisplay::VideoDisplay()
+{
+}
---
+++ src/videodisplay.h
@@ -0,0 +1,10 @@
+#ifndef VIDEODISPLAY_H
+#define VIDEODISPLAY_H
+
+class VideoDisplay
+{
+public:
+    VideoDisplay();
+};
+
+#endif // VIDEODISPLAY_H
